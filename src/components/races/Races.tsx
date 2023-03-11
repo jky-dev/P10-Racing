b1720c798289dc44bd3c87c0fd2672d26d7c5211
Jacky Liang <liangj97@gmail.com>
2023-03-11 20:42:12 +1100

    grab results

--- src/components/races/Races.tsx
+++ src/components/races/Races.tsx
@@ -1,18 +1,20 @@
 import { Card, CardContent, Typography } from '@mui/material'
 import React, { Key, useState } from 'react'
-import { RacesProps } from '../../interfaces'
+import { RaceResult, RacesProps } from '../../interfaces'
 import { fetchPath } from '../../services/database'
 import Loader from '../loader/Loader'
 
 const Races: React.FC = () => {
   const [races, setRaces] = useState<null | RacesProps[]>(null)
+  const [raceResults, setRaceResults] = useState<null | RaceResult[][]>(null)
   const [loading, setLoading] = useState(true)
 
   const init = async () => {
     setLoading(true)
     const dbRaces: Array<RacesProps> = await fetchPath('races')
-
+    const dbResults: Array<Array<RaceResult>> = await fetchPath('results')
     setRaces(dbRaces)
+    setRaceResults(dbResults)
     setLoading(false)
   }
 
@@ -29,9 +31,14 @@ const Races: React.FC = () => {
           <span key={v.round as Key}>
             <Card variant="outlined" sx={{ width: '300px' }}>
               <CardContent>
-                <Typography variant="body1">
-                  {index + 1}: {v.raceName}
-                </Typography>
+                <Typography variant="h6">{v.raceName}</Typography>
+                <Typography variant="body1">Results</Typography>
+                {raceResults[index]?.map((result) => (
+                  <Typography variant="body2">
+                    {result.position}: {result.Driver.givenName}{' '}
+                    {result.Driver.familyName}
+                  </Typography>
+                ))}
               </CardContent>
             </Card>
           </span>
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -6,3 +6,24 @@ export interface RacesProps {
   time: String
   url: String
 }
+
+export interface RaceResult {
+  Constructor: {
+    constructorId: String
+    name: String
+    nationality: String
+    url: String
+  }
+  Driver: {
+    code: String
+    givenName: String
+    familyName: String
+    driverId: String
+  }
+  grid: number
+  laps: number
+  numberpoints: number
+  position: number
+  positionText: String
+  status: String
+}
