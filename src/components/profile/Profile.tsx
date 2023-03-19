71a18207d6eb6de9b49aaa5826c8a564005f79f9
Jacky Liang <liangj97@gmail.com>
2023-03-19 16:18:51 +1100

    change name

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,48 +1,91 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Button, TextField, Typography } from '@mui/material'
 import React from 'react'
 import { useSupabaseContext } from '../../contexts/SupabaseContext'
+import { useUtilsContext } from '../../contexts/UtilsContext'
 import { UserDbProps } from '../../interfaces'
 import Loader from '../loader/Loader'
+import styles from './Profile.module.scss'
 
 const Profile = () => {
   const { user, client } = useSupabaseContext()
   const [profile, setProfile] = React.useState<UserDbProps | null>(null)
   const [loading, setLoading] = React.useState<boolean>(false)
+  const [name, setName] = React.useState('')
+  const { sendAlert } = useUtilsContext()
 
   const fetchProfile = async () => {
+    setLoading(true)
     const { data } = await client.from('users').select('*').eq('uuid', user!.id)
 
-    setProfile((data as UserDbProps[])[0])
+    const profile = (data as UserDbProps[])[0]
+
+    setProfile(profile)
+    setName(profile.name)
     setLoading(false)
   }
 
+  const handleSubmit = async () => {
+    if (name.trim() === profile.name) {
+      sendAlert('Name is already ' + name, 'error')
+      return
+    }
+
+    if (name.trim() === '') {
+      sendAlert('Please enter a name', 'error')
+      return
+    }
+
+    const { error } = await client
+      .from('users')
+      .update({ uuid: user.id, name: name.trim() })
+      .eq('uuid', user.id)
+
+    if (!error) {
+      sendAlert('Name updated to ' + name)
+    } else {
+      sendAlert('An error occurred while updating name', 'error')
+    }
+
+    fetchProfile()
+  }
+
   React.useEffect(() => {
-    setLoading(true)
     fetchProfile()
   }, [user])
 
   return (
-    <>
+    <Box
+      sx={{
+        width: '100%',
+        justifyContent: 'flex-start',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 1,
+      }}
+    >
+      <Typography variant="h4" sx={{ mb: 1 }}>
+        My Profile
+      </Typography>
       {loading || profile === null ? (
         <Loader />
       ) : (
-        <Box
-          sx={{
-            width: '100%',
-            justifyContent: 'flex-start',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: 1,
-          }}
-        >
-          <Typography variant="h4" sx={{ mb: 1 }}>
-            My Profile
-          </Typography>
-          <Typography variant="h5">Email: {profile?.email}</Typography>
-          <Typography variant="h5">Name: {profile?.name}</Typography>
-        </Box>
+        <>
+          <Typography variant="body1">Email: {profile?.email}</Typography>
+          <div className={styles.nameContainer}>
+            <TextField
+              label="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <span>
+              <Button onClick={handleSubmit} variant="contained">
+                Change
+              </Button>
+            </span>
+          </div>
+        </>
       )}
-    </>
+    </Box>
   )
 }
 
